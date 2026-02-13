dddf76cb6b5b590a3a50b7bb00820c2e9f2de588
Basant Raj Sah <47001603+idkanythingbro@users.noreply.github.com>
2026-02-13 21:02:16 +0530

    Create 2191A - Array Coloring.java

---
+++ 2191A - Array Coloring.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String args[]) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t = in.nextInt();
+		while(t-- > 0){
+			solve(in, out);
+		}
+		out.close();
+	}
+	static void solve(FastReader in, PrintWriter out) {
+		int n = in.nextInt();
+		int prev = 0;
+		boolean flag = true;
+		for(int i=1;i<=n;i++){
+		    int val = in.nextInt();
+		    if(i == 1){
+		        prev = val;
+		        continue;
+		    }
+		    if(!flag){
+		        continue;
+		    }
+			if((val + prev) % 2 == 1){
+				flag = true;
+			} else {
+			    flag = false;
+			}
+			prev = val;
+		}		
+		out.println(flag ? "YES" : "NO");
+			
+	}
+
+
+	static class FastReader{
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public FastReader(InputStream is){
+			br = new BufferedReader(new InputStreamReader(is));
+		}
+		
+		String next(){
+			while(st == null || !st.hasMoreElements()){
+				try{
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() { return Integer.parseInt(next()); }
+		long nextLong() { return Long.parseLong(next()); }
+		float nextFloat() { return Float.parseFloat(next()); }
+		double nextDouble() { return Double.parseDouble(next()); }
+		
+		String nextLine(){
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
+	
+	 
