7dd65542371a6e56d4de9becef660218be49e4fc
Basant Raj Sah <47001603+idkanythingbro@users.noreply.github.com>
2026-02-16 15:38:13 +0530

    Create 2195A - Sieve of Erato67henes.java

---
+++ 2195A - Sieve of Erato67henes.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+	public static void main(String args[]) throws Exception {
+		FastReader in = new FastReader(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t = in.nextInt();
+		while(t-- > 0){
+			solve(in, out);
+		}
+		out.close();
+	}
+	static void solve(FastReader in, PrintWriter out) {
+		int n = in.nextInt();
+		boolean flag67 = false;
+		for(int i=0;i<n;i++){
+			int val = in.nextInt();
+			if(val == 67){
+				flag67 = true;
+			}
+		}
+		out.println((flag67) ? "YES" : "NO");		
+	}
+
+
+	static class FastReader{
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public FastReader(InputStream is){
+			br = new BufferedReader(new InputStreamReader(is));
+		}
+		
+		String next(){
+			while(st == null || !st.hasMoreElements()){
+				try{
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() { return Integer.parseInt(next()); }
+		long nextLong() { return Long.parseLong(next()); }
+		float nextFloat() { return Float.parseFloat(next()); }
+		double nextDouble() { return Double.parseDouble(next()); }
+		
+		String nextLine(){
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
+	
+	 
